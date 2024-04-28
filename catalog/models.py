e4a1e64fb3b93d47c20acc022d157a695eb6e691
Ilnara <nara051199@gmail.com>
2024-04-28 20:43:26 +0500

    add changes

---
+++ catalog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils import timezone
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    objects = None
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    objects = None
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    image = models.ImageField(upload_to='catalog/', verbose_name='Изображение', **NULLABLE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(default=timezone.now, verbose_name='Дата изменения')
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+    def __str__(self):
+        return f'{self.name} {self.price}'
